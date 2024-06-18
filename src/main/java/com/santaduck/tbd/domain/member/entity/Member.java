eef3656234acc9365b2ac6fbd658111bddb9da38
Hyejin Hong <hhj732@naver.com>
2024-06-18 21:30:09 +0900

    회원 권한 엔티티 생성

--- src/main/java/com/santaduck/tbd/domain/member/entity/Member.java
+++ src/main/java/com/santaduck/tbd/domain/member/entity/Member.java
@@ -1,30 +1,36 @@
 package com.santaduck.tbd.domain.member.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+@Getter
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(length = 16, nullable = false)
     private String nickname;
-    @Column(length = 20, nullable = false)
+    @Column(length = 256, nullable = false)
     private String password;
     @Column(length = 20, nullable = false)
     private String email;
 
     private Timestamp generatedAt;
     private Timestamp modifiedAt;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private List<Role> roles;
 }
--- src/main/java/com/santaduck/tbd/domain/member/exception/MemberException.java
+++ src/main/java/com/santaduck/tbd/domain/member/exception/MemberException.java
@@ -2,13 +2,12 @@ package com.santaduck.tbd.domain.member.exception;
 
 import com.santaduck.tbd.global.exception.BaseException;
 import com.santaduck.tbd.global.exception.BaseExceptionType;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class MemberException extends BaseException {
     private BaseExceptionType exceptionType;
 
-    public MemberException(BaseExceptionType baseExceptionType) {
-        this.exceptionType = baseExceptionType;
-    }
     @Override
     public BaseExceptionType getExceptionType() {
         return exceptionType;
