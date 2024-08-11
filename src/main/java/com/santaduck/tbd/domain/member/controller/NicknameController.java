c598662104a60a0b1a616f0418db131f20da2f71
Hyejin Hong <hhj732@naver.com>
2024-08-11 13:05:56 +0900

    CORS 설정 추가

--- src/main/java/com/santaduck/tbd/domain/member/controller/NicknameController.java
+++ src/main/java/com/santaduck/tbd/domain/member/controller/NicknameController.java
@@ -11,7 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RestController("/nickname")
+@RestController
+@RequestMapping("/api/nickname")
 @RequiredArgsConstructor
 public class NicknameController {
 
